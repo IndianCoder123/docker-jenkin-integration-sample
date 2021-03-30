8e810eac1dfd1b3a2dbe2b5109b8be112c85b32e
Nilay Sonker <nilaysonker@gmail.com>
2021-03-30 20:25:10 +0530

    Second commit

---
+++ src/test/java/com/javatechie/spring/boot/docker/demo/Test.java
@@ -0,0 +1,9 @@
+package com.javatechie.spring.boot.docker.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Welcome to java techie...!!");
+	}
+
+}
