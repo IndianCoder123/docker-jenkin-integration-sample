29ceb23e465687a7e95029822a0c4a8c60edc653
Nilay Sonker <nilaysonker@gmail.com>
2021-03-30 20:05:28 +0530

    First Commit

---
+++ src/main/java/com/javatechie/spring/boot/docker/demo/DockerJenkinIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.spring.boot.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinIntegrationSampleApplication.class, args);
+	}
+
+}
